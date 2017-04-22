10afbc349de532e70eef5b70e34f2c836fc9d48f
multiopti <gsanchez@usb.ve>
2017-04-22 12:40:34 -0400

    Add files via upload

--- multiopti.py
+++ multiopti.py
@@ -10,8 +10,8 @@ Created on Tue Apr 18 13:09:24 2017
 import numpy as np
 
 def pltc(v1,v2): #classic partially less than
-    lv1 = len(v1)
-    lv2 = len(v2)
+    lv1 = v1.size
+    lv2 = v2.size
     
     if lv1 != lv2:
         print("\n Vector sizes not equal \n")
@@ -19,12 +19,12 @@ def pltc(v1,v2): #classic partially less than
         P1 = np.zeros(lv1)
         P2 = np.zeros(lv1)
         for i in range(0,lv1):
-            if v1[i] <= v2[i]:
+            if v1[0,i] <= v2[0,i]:
                 P1[i] = 1
             else:
                 P1[i] = 0
                 
-            if v1[i] < v2[i]:
+            if v1[0,i] < v2[0,i]:
                 P2[i] = 1
             else:
                 P2[i] = 0
@@ -33,4 +33,26 @@ def pltc(v1,v2): #classic partially less than
         return 1
     else:
         return 0
+
+class VectorSet:
+    data = np.array([])
+    dim = 0
+    size = 0
+    
+    def add(self,V):
+        # Reference the name
+        if self.size == 0:
+            self.data = V
+            self.dim = len(V)
+            self.size = 1
+        else:
+            self.data = np.append(self.data,V,axis = 0)
+            self.size = self.size + 1
+    
+    def disp(self):
+        print("\n")
+        print(self.data)
+        print("\n")
+        print('Size= %d' %self.size)
+
     
